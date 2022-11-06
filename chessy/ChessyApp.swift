53f29c1684936fea7da495965a098dd87c9b17ae
Mike "Koiokos" Kozik <mike.koikos@gmail.com>
2022-11-06 16:56:13 +0100

    Initial commit

---
+++ chessy/ChessyApp.swift
@@ -0,0 +1,12 @@
+//  Created by Michał Kozik on 06/11/2022.
+
+import SwiftUI
+
+@main
+struct ChessyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
